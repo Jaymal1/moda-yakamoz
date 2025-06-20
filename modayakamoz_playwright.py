5361a2c10c56222b868b9514a290362848a88d23
Jaymal1 <63861270+Jaymal1@users.noreply.github.com>
2025-06-20 10:27:08 +0300

    Update modayakamoz_playwright.py

--- modayakamoz_playwright.py
+++ modayakamoz_playwright.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 XML_URL = "https://modayakamoz.com/xml/yalin1"
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
     page = browser.new_page()
     print("Navigating to the XML URL...")
     try:
